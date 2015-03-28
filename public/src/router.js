7a13901f2c67ed2101deab120d8af216b1b6c468
Eray ARSLAN <relfishere@gmail.com>
2015-03-28 11:48:45 +0200

    Error Route Added

--- public/src/router.js
+++ public/src/router.js
@@ -4,26 +4,33 @@
   var App = Backbone.Router.extend({
     routes: {
       '': 'index',
-      ':text': 'hello',
-      ':prefix/:text': 'personal'
-    },
-
-    hello: function (text) {
-      $("#name").html(text.toUpperCase());
-      $("#prefix").html("iPNE");
-      $("title").html(text + "limAmman88");
+      ':text': 'basic',
+      ':prefix/:text': 'advanced',
+      '*path': 'default'
     },
 
     index: function () {
       $("#name").html("BURAK");
-      $("#prefix").html("iPNE");
+      $("#prefix").html("İPNE");
       $("title").html("BurakOnganlimAmman88");
     },
 
-    personal: function (prefix, text) {
+    basic: function (text) {
+      $("#name").html(text.toUpperCase());
+      $("#prefix").html("İPNE");
+      $("title").html(text + "limAmman88");
+    },
+
+    advanced: function (prefix, text) {
       $("#name").html(text.toUpperCase());
       $("#prefix").html(prefix.toUpperCase());
       $("title").html(text + "limAmman88");
+    },
+
+    default: function() {
+      $("#name").html("404");
+      $("#prefix").html("GELEMEDİ");
+      $("title").html("Error" + "lumAmman88");
     }
   });
 
