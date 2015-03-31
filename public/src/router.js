99277fc0913c762b15d1e6d0ab15524cb50d86b0
Eray ARSLAN <relfishere@gmail.com>
2015-03-31 21:28:45 +0300

    og added

--- public/src/router.js
+++ public/src/router.js
@@ -11,24 +11,48 @@ define(['jquery', 'backbone'], function($, Backbone) {
       $("#name").html("BURAK");
       $("#prefix").html("İPNE");
       $("title").html("BurakOnganlimAmman88");
+      //
+      $('meta[property="og:title"]').remove();
+      $('head').append( '<meta name="og:title" content="'+'BurakOnganlimAmman88'+'">' );
+
+      $('meta[property="og:description"]').remove();
+      $('head').append( '<meta name="og:description" content="'+'İPNE' + ' ' + 'BURAK' +'">' );
     },
 
     basic: function (text) {
       $("#name").html(text.toUpperCase());
       $("#prefix").html("İPNE");
       $("title").html(text + "limAmman88");
+      //
+      $('meta[property="og:title"]').remove();
+      $('head').append( '<meta name="og:title" content="'+text + 'limAmman88'+'">' );
+
+      $('meta[property="og:description"]').remove();
+      $('head').append( '<meta name="og:description" content="' + 'İPNE' + ' ' + text.toUpperCase() +'">' );
     },
 
     advanced: function (prefix, text) {
       $("#name").html(text.toUpperCase());
       $("#prefix").html(prefix.toUpperCase());
       $("title").html(text + "limAmman88");
+      //
+      $('meta[property="og:title"]').remove();
+      $('head').append( '<meta name="og:title" content="'+text + 'limAmman88'+'">' );
+
+      $('meta[property="og:description"]').remove();
+      $('head').append( '<meta name="og:description" content="'+prefix.toUpperCase() + ' ' + text.toUpperCase() +'">' );
     },
 
     default: function() {
       $("#name").html("404");
       $("#prefix").html("GELEMEDİ");
       $("title").html("Error" + "lumAmman88");
+      //
+      $('meta[property="og:title"]').remove();
+      $('head').append( '<meta name="og:title" content="'+'ErrorlumAmman88'+'">' );
+
+      $('meta[property="og:description"]').remove();
+      $('head').append( '<meta name="og:description" content="'+'GELEMEDİ' + ' ' + '404' +'">' );
     }
   });
 });
