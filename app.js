1c2fb41af744bd110fb315f3ac7a1644d479c89a
Eray ARSLAN <relfishere@gmail.com>
2015-04-17 12:23:35 +0300

    useless commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var app = express();
 var fileSystem = require('fs');
+var sitemap = require('express-sitemap');
 var logFile = fileSystem.createWriteStream('log.txt', {flags: 'a'});
 
 app.configure(function () {
@@ -13,8 +14,20 @@ app.configure(function () {
   app.use(express.logger({stream: logFile}));
 });
 
+app.get('/sitemap.xml', function(request, response) {
+  map.XMLtoWeb(response);
+});
+
+app.get('/robots.txt', function(request, response) {
+  map.TXTtoWeb(response);
+});
+
 app.get('*', function (request, response) {
   response.sendfile(__dirname + '/public/index.html');
+});
+
+var map = sitemap({
+  generate: app
 });
 
 app.listen(process.env.PORT || 4141);
